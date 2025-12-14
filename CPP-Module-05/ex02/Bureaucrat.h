f07eb63bd581a7afcdfcc651cadcf287b74ce7c3
Mehdi Bounouif <mbounoui@c1r6p3.1337.ma>
2025-12-14 10:08:19 +0100

    Module-05 ex02 started

---
+++ CPP-Module-05/ex02/Bureaucrat.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbounoui <mbounoui@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 09:25:43 by mbounoui          #+#    #+#             */
+/*   Updated: 2025/12/13 09:08:32 by mbounoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _BUREAUCRAT_
+#define _BUREAUCRAT_
+
+#include <iostream>
+
+class Form;
+
+class	Bureaucrat
+{
+	const	std::string name;
+	int		grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name, int grade);
+		Bureaucrat(const Bureaucrat &obj);
+		Bureaucrat & operator=(const Bureaucrat &obj);
+		const std::string getName() const;
+		int getGrade() const;
+		void	increment();
+		void	decrement();
+		class	GradeTooHighException : public std::exception
+		{
+			const char *what() const throw();
+		};
+		class	GradeTooLowException: public std::exception
+		{
+			const char *what() const throw();
+		};
+		void	signForm(Form &obj);
+		~Bureaucrat();
+};
+
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &obj);
+
+#endif
