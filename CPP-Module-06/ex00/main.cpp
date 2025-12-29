a3f5f58780ac2ca4735debfce79cc3cf9d77cffb
Mehdi Bounouif <mbounoui@c1r6p7.1337.ma>
2025-12-29 13:21:28 +0100

    check digits

--- CPP-Module-06/ex00/main.cpp
+++ CPP-Module-06/ex00/main.cpp
@@ -6,15 +6,30 @@
 /*   By: mbounoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 21:47:35 by mbounoui          #+#    #+#             */
-/*   Updated: 2025/12/28 22:40:53 by mbounoui         ###   ########.fr       */
+/*   Updated: 2025/12/29 13:20:45 by mbounoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	onlyDigits(const std::string &a)
+int	onlyDigits(const std::string &a, int index)
 {
-	return (a.find_first_not_of("0123456789") == std::string::npos);
+	return (a.find_first_not_of("0123456789", index) == std::string::npos);
+}
+
+bool checkDecimal(const std::string &str)
+{
+	int index;
+
+	index = 0;
+	if (str.find('-') != 0 && str.find('-') != std::string::npos)
+		return (false);
+	else if (str.find('-') == 0)
+		index = 1;
+	if (!onlyDigits(str, index) || str.empty())
+		return (false);
+	std::cout << str << std::endl;
+	return (true);
 }
 
 bool checkFloat(const std::string &a)
@@ -43,7 +58,7 @@ bool checkFloat(const std::string &a)
 	std::string decimal = a.substr(index, dot);
 	std::string fraction = a.substr(dot + 1, 1);
 
-	if (!onlyDigits(decimal) || !onlyDigits(fraction))
+	if (!onlyDigits(decimal,0) || !onlyDigits(fraction,0))
 		return (false);
 
 	std::cout << decimal << std::endl;
@@ -53,9 +68,12 @@ bool checkFloat(const std::string &a)
 
 int	main()
 {
-	std::string a = "1.1f";
+	std::string a = "11.2f";
+	std::string b = "-39232093902";
 
 	checkFloat(a);
+	std::cout << "==============\n";
+	checkDecimal(b);
 //	const size_t signal = a.find('-');
 //	// found but not in the start
 //	if (signal != 0 && signal != std::string::npos)
