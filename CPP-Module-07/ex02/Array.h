0d6f2984c6557e9ccb408f7fabd63e7fb403a16d
Mehdi Bounouif <mbounoui@c1r6p7.1337.ma>
2026-01-08 11:58:40 +0100

    some mode

--- CPP-Module-07/ex02/Array.h
+++ CPP-Module-07/ex02/Array.h
@@ -26,6 +26,68 @@ class Array
   unsigned int size() const;
 };
 
-#include "Array.tpp"
+template <typename T>
+Array<T>::Array(){
+    _size = 0;
+    data = NULL;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n){
+	_size = n;
+	data = new T[n]();
+
+}
+
+template <typename T>
+Array<T>::Array(const Array<T> &obj)
+{
+	_size = obj._size;
+	data = new T[_size]();
+	for (unsigned int i = 0; i < _size; i++)
+	{
+		data[i] = obj.data[i];
+	}
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &obj){
+  if (this == &obj)
+    return (*this);
+  delete [] this->data;
+
+  this->_size = obj._size;
+  this->data = new T[obj._size]();
+  
+  for (unsigned int i = 0; i < obj._size; i++)
+    this->data[i] = obj.data[i];
+  return (*this);
+}
+
+template <typename T>
+Array<T>::~Array(){
+  delete [] this->data;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int n){
+  if (this->_size <= n)
+    throw std::exception();
+  else 
+  return (this->data[n]);
+}
+
+template <typename T>
+const T &Array<T>::operator[](unsigned int n) const{
+  if (this->_size <= n)
+    throw std::exception();
+  else 
+    return (this->data[n]);
+}
+
+template <typename T>
+unsigned int Array<T>::size() const{
+  return (this->_size);
+}
 
 #endif
