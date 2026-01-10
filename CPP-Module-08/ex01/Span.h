f4dceda27e524f6f4a6cb87b47aca4dbf88e5ec6
Mehdi Bounouif <mbounoui@c1r6p7.1337.ma>
2026-01-10 15:48:00 +0100

    make structurec

--- CPP-Module-08/ex01/Span.h
+++ CPP-Module-08/ex01/Span.h
@@ -6,13 +6,65 @@
 /*   By: mbounoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 14:54:10 by mbounoui          #+#    #+#             */
-/*   Updated: 2026/01/10 14:55:02 by mbounoui         ###   ########.fr       */
+/*   Updated: 2026/01/10 15:42:17 by mbounoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _SPAN_
 #define _SPAN_
 
-class Span{};
+#include <iostream>
+#include <vector>
+
+class Span
+{
+	unsigned int size;
+	std::vector<int> data;
+
+	public:
+		Span(unsigned int n);
+		void	addNumber(int n);
+		int		shortestSpan();
+		int		longestSpan();
+};
+
+
+Span::Span(unsigned int n)
+{
+	size = n;
+	data = new int[n];
+	for (unsigned int i = 0; i < n; i++)
+		data[i] = 0;
+}
+
+void	Span::addNumber(int n)
+{
+	unsigned int i = 0;
+
+	while (data[i] != 0)
+		i++;
+	
+	if (i == size)
+		throw std::exception();
+
+	data[i] = n;
+}
+
+int Span::shortestSpan()
+{
+	std::sort(data.begin(), data.end());
+	return (data[1] - data[0]);
+
+//	for (int i = 0; i < data.size(); i++)
+//		std::cout << data[i] << std::endl;
+}
+
+int Span::longestSpan()
+{
+	std::sort(data.begin(), data.end());
+	return (data.end() - data.begin());
+//	for (int i = 0; i < data.size(); i++)
+//		std::cout << data[i] << std::endl;
+}
 
 #endif
