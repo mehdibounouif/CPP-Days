43b91564e2af0a18e6b0ec6cf5abadadb85a8e81
mehdi bounouif <mehdibonouif@gmail.com>
2026-01-12 15:00:29 +0100

    CPP-Module-08 ex02 finished

--- CPP-Module-08/ex02/Mutantstack.h
+++ CPP-Module-08/ex02/Mutantstack.h
@@ -13,6 +13,86 @@
 #ifndef _MUTANTSTACK_
 #define _MUTANTSTACK_
 
-class MutantStack{};
+#include <stack>
+#include <iostream>
+#include <deque>
 
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    // Orthodox canonical form (good practice)
+    MutantStack() {}
+    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    MutantStack& operator=(const MutantStack& other)
+    {
+        std::stack<T>::operator=(other);
+        return *this;
+    }
+    ~MutantStack() {}
+
+    // Iterator types
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+    // Iterator functions
+    iterator begin() { return this->c.begin(); }
+    iterator end()   { return this->c.end(); }
+
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end()   const { return this->c.end(); }
+};
+/*
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(){}
+		MutantStack(const MutantStack &obj): std::stack<T>(obj)
+		{}
+		MutantStack(const MutantStack &obj)
+		{
+			std::stack<T>::operator=(obj);
+			return (*this);
+		}
+		~MutantStatck(){}
+		typedef	typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		iterator begin() const {return (this->c.begin());}
+		iterator end() const {return (this->c.end());}
+
+		const_iterator begin() const {return (this->c.begin());}
+		const_iterator end() const {return (this->c.end());}
+};
+*/
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
